9486897b37d88745f25578dfd1b966f99bc13c31
Reynaldo Jarro <reyrjarroneto@gmail.com>
2025-03-03 12:26:53 -0800

    actually removing bad input

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -27,24 +27,3 @@ output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
   value       = module.compute.ecs_cluster_name
 }
-
-output "deployment_command" {
-  description = "Command to deploy a new image to ECR"
-  value       = <<-EOT
-    # Build and push the container image
-    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin $(aws sts get-caller-identity --query Account --output text).dkr.ecr.${var.aws_region}.amazonaws.com
-    docker build -t ${module.compute.ecr_repository_url}:latest .
-    docker push ${module.compute.ecr_repository_url}:latest
-  EOT
-}
-
-output "run_command" {
-  description = "Command to run the producer application"
-  value       = <<-EOT
-    # Set the STATE_MACHINE_ARN environment variable
-    export STATE_MACHINE_ARN="${module.orchestration.state_machine_arn}"
-    
-    # Run the producer application with a targets file
-    go run cmd/producer/main.go -file=targets.txt
-  EOT
-}
