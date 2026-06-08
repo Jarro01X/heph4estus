1d252ca0109f6d4528bdd5b489453c9b91b1f525
Jarro01X <reyrjarroneto@gmail.com>
2026-06-07 19:18:05 -0700

    chore(aws): add terraform provider constraints

--- deployments/aws/generic/messaging/main.tf
+++ deployments/aws/generic/messaging/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 # Main SQS queue for tasks
 resource "aws_sqs_queue" "tasks" {
   name                       = "${var.name_prefix}-tasks"
